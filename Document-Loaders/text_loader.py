ed54f5a6bf8c73f67e396de30c5e66fd5a26bfcf
Shivam <shivam.vats1204@gmail.com>
2025-06-27 18:55:24 +0530

    Created Document-Loaders folder

---
+++ Document-Loaders/text_loader.py
@@ -0,0 +1,6 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
