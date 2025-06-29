7f70297945409597f966435b496739c45c110b15
Shivam <shivam.vats1204@gmail.com>
2025-06-29 16:35:31 +0530

    added resume creation using ChatGroq

--- Document-Loaders/webbase_loader.py
+++ Document-Loaders/webbase_loader.py
@@ -1,4 +1,10 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 url="https://shivamvats.netlify.app/"
 loader = WebBaseLoader(url)
@@ -7,3 +13,15 @@ docs = loader.load()
 print(len(docs))
 print("\n Website's content is:", docs[0].page_content, sep="\n")
 
+# Creating Resume using Groq's Chat model
+model = ChatGroq(model_name="mistral-saba-24b")
+prompt=PromptTemplate(
+    template="Create a resume for the following profile: \n{profile}",
+    input_variables=["profile"],
+)
+
+parser=StrOutputParser()
+
+chain = prompt | model | parser
+
+print("Resume created:",chain.invoke({"profile":docs[0].page_content}),sep="\n")
