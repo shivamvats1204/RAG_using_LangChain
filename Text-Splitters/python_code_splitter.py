21534fb2e736cbff72c560b929dc17ecddafee11
Shivam <shivam.vats1204@gmail.com>
2025-07-02 10:24:25 +0530

    coded python_code_splitter.py

---
+++ Text-Splitters/python_code_splitter.py
@@ -0,0 +1,24 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
+text="""
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+"""
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON,
+    chunk_size=200,
+    chunk_overlap=0,
+)
+
+chunks=splitter.split_text(text)
+print(len(chunks))  # Print the number of chunks created
+print(chunks)  # Print the chunks
